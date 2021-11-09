1d704703a6f8efe6c033b2609d59fd850e814fcc
august110th <august110th@ya.ru>
2021-11-10 00:07:12 +0400

    done

---
+++ 1.2-requests-templates/pagination/pagination/test.py
@@ -0,0 +1,10 @@
+import csv
+
+
+
+with open('C:\data-398-2018-08-30.csv', encoding='Utf-8') as f:
+    file_reader = csv.DictReader(f)
+#     for row in file_reader:
+#         row = tuple(row)
+#         print(type(row))
+    print(type())
--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -10,8 +13,15 @@ def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
+    f = open('C:\data-398-2018-08-30.csv', encoding='Utf-8', newline='')
+    file_reader = csv.DictReader(f)
+    content = list(file_reader)
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(content, 10)
+    page = paginator.get_page(current_page)
+
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': content,
+        'page': page
     }
     return render(request, 'stations/index.html', context)
