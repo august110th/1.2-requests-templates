dcbbc002dec7c18f241800f15f9a7c9f19b0e648
august110th <110THkamamber>
2021-11-10 19:11:32 +0400

    done

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -11,16 +11,16 @@ def index(request):
 
 def bus_stations(request):
 
-    f = open(settings.BUS_STATION_CSV, encoding='Utf-8', newline='')
-    file_reader = csv.DictReader(f)
-    content = list(file_reader)
-    current_page = int(request.GET.get('page', 1))
-    paginator = Paginator(content, 10)
-    page = paginator.get_page(current_page)
-    page_content = page.object_list
+    with open(settings.BUS_STATION_CSV, encoding='Utf-8', newline='') as f:
+        file_reader = csv.DictReader(f)
+        content = list(file_reader)
+        current_page = int(request.GET.get('page', 1))
+        paginator = Paginator(content, 10)
+        page = paginator.get_page(current_page)
+        page_content = page.object_list
 
-    context = {
-        'bus_stations': page_content,
-        'page': page
-    }
-    return render(request, 'stations/index.html', context)
+        context = {
+            'bus_stations': page_content,
+            'page': page
+        }
+        return render(request, 'stations/index.html', context)
